7838aee819f21e32bf4cb61ee185122ab3a990f4
S <andre.serudo@meliuz.com.br>
2023-03-04 21:55:18 -0400

    feat: Add field validation

--- src/modules/artists/actions/create.js
+++ src/modules/artists/actions/create.js
@@ -1,4 +1,5 @@
 const Artist = require('../model/Artist');
+const { Error } = require('mongoose')
 
 const createAction = async (req, res) => {
     const artist = req.body;
@@ -9,8 +10,15 @@ const createAction = async (req, res) => {
         res.status(201).send()
         return
     }catch (error){
-        res.status(500).send({message: 'something went wrong'})
-        return
+        if (error instanceof Error.ValidationError) {
+            const {name} = error.errors;
+
+            res.status(400).send({message: error.message})
+            return
+          } else {
+            res.status(500).send({message: 'something went wrong'})
+            return
+        }
     }
 }
 
