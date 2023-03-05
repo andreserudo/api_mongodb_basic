90cae488726198325aae947b1e2783220964de70
S <andre.serudo@meliuz.com.br>
2023-03-04 21:55:38 -0400

    test: Add field validation

--- src/modules/artists/__tests__/actions/create.test.js
+++ src/modules/artists/__tests__/actions/create.test.js
@@ -1,6 +1,7 @@
 const Artist = require('../../model/Artist');
 const createAction = require('../../actions/create')
 const mockedCreate = require('../mocks/create.test.json');
+const { Error } = require('mongoose')
 
 describe('POST /artist', () => {
     describe('when request is sucessfully', () => {
@@ -25,6 +26,18 @@ describe('POST /artist', () => {
 
             expect(res.status).toHaveBeenCalledWith(500);
             expect(res.send).toHaveBeenCalledWith({message: "something went wrong"});    
+        });        
+    });
+    describe('when request body is not correct', () => {
+        it('should throw a validation error', async () => {
+            const req = { body: {} }
+            const res = { status: jest.fn().mockReturnThis(), send: jest.fn() };
+            Artist.create = jest.fn(() => { throw new Error.ValidationError()});
+
+            await createAction(req, res);
+
+            expect(res.status).toHaveBeenCalledWith(400);
+            expect(res.send).toHaveBeenCalledWith({ message: 'Validation failed'});    
         });        
     });
 });
